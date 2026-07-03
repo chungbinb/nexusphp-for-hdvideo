a68c48b608eab1e3867ecca872106b1b58d087e7
chungbinb <binou_binglong@163.com>
2026-07-03 17:07:37 +0700

    Remove empty mobile staff spacer cards

--- public/staff.php
+++ public/staff.php
@@ -7,7 +7,7 @@ loggedinorreturn(true);
 user_can('staffmem', true);
 mp_head($lang_staff['head_staff']);
 
-$Cache->new_page(mobile_is() ? 'staff_page_mobile_20260703d' : 'staff_page_20260703', 900, true);
+$Cache->new_page(mobile_is() ? 'staff_page_mobile_20260703e' : 'staff_page_20260703', 900, true);
 if (!$Cache->get_page()){
 $Cache->add_whole_row();
 begin_main_frame();
@@ -145,7 +145,7 @@ while ($arr = mysql_fetch_assoc($res))
 	if($curr_class != $arr['class'])
 	{
 		$curr_class = $arr['class'];
-		if ($ppl != "")
+		if ($ppl != "" && !mobile_is())
 			$ppl .= "<tr height=15><td class=embedded colspan=5 align=right>&nbsp;</td></tr>";
 		$ppl .= "<tr height=15><td class=embedded colspan=5 align=right>" . get_user_class_name($arr["class"],false,true,true) . "</td></tr>";
 		$ppl .= "<tr>" .
