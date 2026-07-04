0454cd6391999d0fc79186ad631dfd071347d05c
chungbinb <binou_binglong@163.com>
2026-07-04 11:23:42 +0700

    Improve mobile unconfirmed users layout

--- public/unco.php
+++ public/unco.php
@@ -5,7 +5,7 @@ loggedinorreturn();
 
 if (get_user_class() < UC_MODERATOR)
 stderr("Sorry", "Access denied.");
-$status = $_GET['status'];
+$status = $_GET['status'] ?? 0;
 	if ($status)
 		int_check($status,true);
 		
@@ -13,34 +13,39 @@ $res = sql_query("SELECT * FROM users WHERE status='pending' ORDER BY username" 
 if( mysql_num_rows($res) != 0 )
 {
 	stdhead("Unconfirmed Users");
+	print '<div class="unco-page">';
+	print '<h1 class="unco-title">Unconfirmed Users</h1>';
 	begin_main_frame();
 	begin_frame("");
-print'<br><table width=100% border=1 cellspacing=0 cellpadding=5>';
-if ($status)
-	print '<tr><td class=rowhead colspan=5><font color=red size=1>The User account has been updated!</font></tr></td>';
-print'<tr>';
+print'<table class="unco-table" width=100% border=1 cellspacing=0 cellpadding=5>';
+print'<thead><tr>';
 print'<td class=rowhead><center>Name</center></td>';
 print'<td class=rowhead><center>eMail</center></td>';
 print'<td class=rowhead><center>Added</center></td>';
 print'<td class=rowhead><center>Set Status</center></td>';
 print'<td class=rowhead><center>Confirm</center></td>';
-print'</tr>';
+print'</tr></thead><tbody>';
+if ($status)
+	print '<tr class="unco-status-row"><td class="unco-status" colspan=5>The User account has been updated!</td></tr>';
 while( $row = mysql_fetch_assoc($res) )
 {
-$id = $row['id'];
-print'<tr><form method=post action=modtask.php>';
-print'<input type=hidden name=\'action\' value=\'confirmuser\'>';
+$id = (int)$row['id'];
+$formId = 'unco-confirm-' . $id;
+print'<tr class="unco-row">';
+print'<td class="unco-user" data-label="Name"><center>' . get_username($id) . '</center></td>';
+print'<td data-label="eMail" align=center>' . htmlspecialchars($row['email']) . '</td>';
+print'<td data-label="Added" align=center>' . htmlspecialchars($row['added']) . '</td>';
+print'<td data-label="Set Status" align=center><select name=confirm form="' . $formId . '"><option value=pending>pending</option><option value=confirmed>confirmed</option></select></td>';
+print'<td data-label="Confirm" align=center><form id="' . $formId . '" class="unco-confirm-form" method=post action=modtask.php>';
+print'<input type=hidden name="action" value="confirmuser">';
 print("<input type=hidden name='userid' value='$id'>");
-print'<a href="userdetails.php?id=' . $row['id'] . '"><td><center>' . $row['username'] . '</center></td></a>';
-print'<td align=center>&nbsp;&nbsp;&nbsp;&nbsp;' . $row['email'] . '</td>';
-print'<td align=center>&nbsp;&nbsp;&nbsp;&nbsp;' . $row['added'] . '</td>';
-print'<td align=center><select name=confirm><option value=pending>pending</option><option value=confirmed>confirmed</option></select></td>';
-print'<td align=center><input type=submit value="-Go-" style=\'height: 20px; width: 40px\'>';
-print'</form></tr>';
+print'<input class="btn" type=submit value="-Go-">';
+print'</form></td></tr>';
 }
-print '</table>';
+print '</tbody></table>';
 end_frame();
 end_main_frame();
+print '</div>';
 }else{
 	if ($status) {
 		stderr("Updated!","The user account has been updated.");
