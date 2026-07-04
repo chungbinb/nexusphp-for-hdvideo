ddae362c9b936241cb408c2775c1b814c8827382
chungbinb <binou_binglong@163.com>
2026-07-04 07:49:53 +0700

    Separate mobile staffbox status row

--- public/staffbox.php
+++ public/staffbox.php
@@ -79,9 +79,9 @@ if (!$action) {
                 print("<article class=\"staffbox-card $answeredClass\">");
                 print("<label class=\"staffbox-select\"><input type=\"checkbox\" name=\"setanswered[]\" value=\"" . (int)$arr['id'] . "\" /><span></span></label>");
                 print("<a class=\"staffbox-card-body\" href=\"".htmlspecialchars($viewUrl)."\">");
-                print("<span class=\"staffbox-subject\">".htmlspecialchars($arr['subject'])."</span>");
-                print("<span class=\"staffbox-meta\"><span class=\"staffbox-meta-row\"><span class=\"staffbox-meta-label\">".$lang_staffbox['col_sender']."</span><span class=\"staffbox-meta-value\">" . get_username($arr['sender']) . "</span></span><span class=\"staffbox-meta-row staffbox-time\"><span class=\"staffbox-meta-label\">".$lang_staffbox['col_added']."</span><span class=\"staffbox-meta-value\">".gettime($arr['added'], true, false)."</span></span></span>");
-                print("<span class=\"staffbox-answer\"><b>".$lang_staffbox['col_answered']." ".$answeredLabel."</b>".($answeredBy ? " <span>".$answeredBy."</span>" : "")."</span>");
+                print("<div class=\"staffbox-subject\">".htmlspecialchars($arr['subject'])."</div>");
+                print("<div class=\"staffbox-meta\"><div class=\"staffbox-meta-row\"><span class=\"staffbox-meta-label\">".$lang_staffbox['col_sender']."</span><span class=\"staffbox-meta-value\">" . get_username($arr['sender']) . "</span></div><div class=\"staffbox-meta-row staffbox-time\"><span class=\"staffbox-meta-label\">".$lang_staffbox['col_added']."</span><span class=\"staffbox-meta-value\">".gettime($arr['added'], true, false)."</span></div></div>");
+                print("<div class=\"staffbox-answer\"><b>".$lang_staffbox['col_answered']." ".$answeredLabel."</b>".($answeredBy ? " <span>".$answeredBy."</span>" : "")."</div>");
                 print("</a>");
                 print("</article>\n");
             } else {
