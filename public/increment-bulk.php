f5b497a2333e5a3e93c34ebee94468c31e44e97f
chungbinb <binou_binglong@163.com>
2026-07-04 09:16:36 +0700

    Improve mobile increment bulk layout

--- public/increment-bulk.php
+++ public/increment-bulk.php
@@ -11,10 +11,10 @@ $type = $_REQUEST['type'] ?? '';
 stdhead($lang_incrementbulk['page_title'], false);
 $classes = array_chunk(\App\Models\User::listClass(), 4, true);
 ?>
-    <table class=main width=737 border=0 cellspacing=0 cellpadding=0><tr><td class=embedded>
-                <div align=center>
-                    <h1><?php echo $lang_incrementbulk['page_title']?></a></h1>
-                    <form method=post action=take-increment-bulk.php>
+    <table class="main increment-bulk-shell" width=737 border=0 cellspacing=0 cellpadding=0><tr><td class=embedded>
+                <div align=center class="increment-bulk-wrap">
+                    <h1><?php echo $lang_incrementbulk['page_title']?></h1>
+                    <form method=post action=take-increment-bulk.php class="increment-bulk-form">
                         <?php
 
                         if (isset($_GET["returnto"]) || $_SERVER["HTTP_REFERER"])
@@ -24,22 +24,22 @@ $classes = array_chunk(\App\Models\User::listClass(), 4, true);
                             <?php
                         }
                         ?>
-                        <table cellspacing=0 cellpadding=5>
+                        <table cellspacing=0 cellpadding=5 class="increment-bulk-table">
                             <?php
                             if (isset($_GET["sent"]) && $_GET["sent"] == 1) {
-                                echo '<tr><td colspan=2 class="text" align="center"><font color=red><b> '. ($validTypeMap[$type] ?? '') . $lang_incrementbulk['sent_success'] .'</font></b></tr></td>';
+                                echo '<tr><td colspan=2 class="text increment-bulk-alert" align="center"><font color=red><b> '. ($validTypeMap[$type] ?? '') . $lang_incrementbulk['sent_success'] .'</b></font></td></tr>';
                             }
                             ?>
                             <tr>
                                 <td class="rowhead" valign="top"><?php echo $lang_incrementbulk['labels']['type'] ?></td>
-                                <td class="rowfollow">
+                                <td class="rowfollow increment-type-options">
                                     <?php
                                     foreach ($validTypeMap as $name => $text) {
                                         $desc = '';
                                         if ($name == 'uploaded') {
                                             $desc = '&nbsp;(GB)';
                                         }
-                                        printf('<label><input type="radio" name="type" value="%s">%s%s</label>', $name, $text, $desc);
+                                        printf('<label class="increment-choice"><input type="radio" name="type" value="%s"><span>%s%s</span></label>', $name, $text, $desc);
                                     }
                                     ?>
                                 </td>
@@ -48,28 +48,26 @@ $classes = array_chunk(\App\Models\User::listClass(), 4, true);
                             <tr><td class="rowhead" valign="top"><?php echo $lang_incrementbulk['labels']['duration'] ?></td><td class="rowfollow"><input type=number min="1" name=duration size=10> <?php echo $lang_incrementbulk['labels']['duration_help'] ?></td></tr>
                             <tr>
                                 <td class="rowhead" valign="top"><?php echo $lang_incrementbulk['labels']['user_class'] ?></td><td class="rowfollow">
-                                    <table style="border: 0" width="100%" cellpadding="0" cellspacing="0">
+                                    <div class="increment-class-grid">
                                         <?php
                                         foreach ($classes as $chunk) {
-                                            printf('<tr>');
                                             foreach ($chunk as $class => $info) {
-                                                printf('<td style="border: 0"><label><input type="checkbox" name="classes[]" value="%s" />%s</label></td>', $class, $info);
+                                                printf('<label class="increment-choice"><input type="checkbox" name="classes[]" value="%s" /><span>%s</span></label>', $class, $info);
                                             }
-                                            printf('</tr>');
                                         }
                                         ?>
-                                    </table>
+                                    </div>
                                 </td>
                             </tr>
                             <?php do_action('form_role_filter', $lang_incrementbulk['labels']['roles']) ?>
                             <tr><td class="rowhead" valign="top"><?php echo $lang_incrementbulk['labels']['msg_subject'] ?> </td><td class="rowfollow"><input type=text name=subject size=82></td></tr>
                             <tr><td class="rowhead" valign="top"><?php echo $lang_incrementbulk['labels']['msg_body'] ?> </td><td class="rowfollow"><textarea name=msg cols=80 rows=5><?php echo $body ?? ''?></textarea></td></tr>
                             <tr>
-                                <td class="rowfollow" colspan=2><div align="center"><b><?php echo $lang_incrementbulk['labels']['operator'] ?>:&nbsp;&nbsp;</b>
+                                <td class="rowfollow increment-operator" colspan=2><div align="center" class="increment-operator-box"><b><?php echo $lang_incrementbulk['labels']['operator'] ?>:&nbsp;&nbsp;</b>
                                         <label><input name="sender" type="radio" value="self" checked><?php echo $CURUSER['username']?></label>
                                         &nbsp; <label><input name="sender" type="radio" value="system">System</label>
                                     </div></td></tr>
-                            <tr><td class="rowfollow" colspan=2 align=center><input type=submit value="<?php echo nexus_trans('label.submit') ?>" class=btn></td></tr>
+                            <tr><td class="rowfollow increment-submit" colspan=2 align=center><input type=submit value="<?php echo nexus_trans('label.submit') ?>" class=btn></td></tr>
                         </table>
                         <input type=hidden name=receiver value=<?php echo $receiver ?? ''?>>
                     </form>
